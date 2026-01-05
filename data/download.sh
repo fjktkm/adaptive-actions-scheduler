0feae97f9f752216c323cb84163f28fcc21ee4c6
fjktkm <fjktkm@gmail.com>
2026-01-05 19:30:35 +0000

    スケジュールイベントのみをフィルタリングするようにダウンロードスクリプトを修正

--- data/download.sh
+++ data/download.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 cd "$(dirname "$0")"
 
 echo "cron,startedAt" > data.csv
-gh run list --json headSha,startedAt --limit 100 | \
-  jq -r '.[] | .startedAt + " " + .headSha' | \
+gh run list --json headSha,startedAt,event --limit 100 | \
+  jq -r '.[] | select(.event == "schedule") | .startedAt + " " + .headSha' | \
   while read time sha; do
     cron=$(gh api "repos/{owner}/{repo}/contents/.github/workflows/schedule.yml?ref=$sha" \
       --jq '.content | @base64d | match("cron: \"(.*)\"") | .captures[0].string' 2>/dev/null)
